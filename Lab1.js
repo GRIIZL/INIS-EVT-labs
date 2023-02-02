08449eb96b88a6fb1bceec7af10ac7541508e2dc
GRIIZL <72096275+GRIIZL@users.noreply.github.com>
2023-02-02 20:32:14 +0300

    Update Lab1.js

---
+++ Lab1.js
@@ -0,0 +1,21 @@
+let numberOfFilms = prompt("Сколько фильмов вы уже посмотрели");
+const personalMovieDB = 
+{
+    count: numberOfFilms, 
+    movies: {}
+};
+
+for (var i=0; i<2; i++)
+{
+	let film = prompt("Один из последних просмотренных фильмов?")
+	let rate = prompt("На сколько оцените его?")
+	if (film!=null && film!="" && film.length < 50)
+	{
+	personalMovieDB.movies[film]=rate;
+	}
+	else
+	{
+		alert("Что-то пошло не так");
+	}
+}
+console.log(personalMovieDB)
