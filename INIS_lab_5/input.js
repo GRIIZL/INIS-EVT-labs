8213ad30da28fec8f08333d699e7026ad7814c6f
Matvey <72096275+GRIIZL@users.noreply.github.com>
2023-04-26 02:46:02 +0300

    Add files via upload

---
+++ INIS_lab_5/input.js
@@ -0,0 +1,53 @@
+const targets = document.querySelectorAll('.target');
+
+let isDragging = false;
+let startX, startY;
+
+targets.forEach(target => {
+    target.setAttribute('tabindex', '0');
+
+
+
+    target.addEventListener('mousedown', e => {
+      isDragging = true;
+      startX = e.clientX - target.offsetLeft;
+      startY = e.clientY - target.offsetTop;
+      lastX = target.offsetLeft;
+      lastY = target.offsetTop;
+    });
+
+
+  target.addEventListener('mousemove', e => {
+    if (isDragging) {
+      const x = e.clientX;
+      const y = e.clientY;
+      target.style.left = `${x - startX}px`;
+      target.style.top = `${y - startY}px`;
+    }
+  });
+
+  target.addEventListener('mouseup', e => {
+    isDragging = false;
+  });
+
+  target.addEventListener('keydown', e => {
+    if (e.key === 'Escape' && isDragging) {
+      isDragging = false;
+      target.style.left = `${lastX}px`;
+      target.style.top = `${lastY}px`;
+    }
+  });
+  
+
+  target.addEventListener('dblclick', e => {
+    isDragging = true;
+    startX = e.clientX - target.offsetLeft;
+    startY = e.clientY - target.offsetTop;
+    target.style.backgroundColor = 'blue';
+  });
+
+  target.addEventListener('click', e => {
+    isDragging = false;
+    target.style.backgroundColor = 'red';
+  });
+});
