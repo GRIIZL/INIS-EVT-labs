f7e9af006f442882c84c306cd6c849db6f5d85a3
Matvey <72096275+GRIIZL@users.noreply.github.com>
2023-04-26 22:55:37 +0300

    Add files via upload

---
+++ INIS_lab_6/input.js
@@ -0,0 +1,73 @@
+// Получаем все элементы с классом "target"
+const targets = document.querySelectorAll('.target');
+let isDoubleClick = false;
+let touchTimeout = null;
+let currentTarget = null;
+
+
+
+// Обрабатываем каждый элемент
+targets.forEach(target => {
+  let startX, startY;
+
+
+  target.addEventListener('touchstart', function(e) {
+    const touch = e.changedTouches[0];
+    if (touchTimeout !== null) {
+      clearTimeout(touchTimeout);
+      touchTimeout = null;
+      isDoubleClick=true;
+      e.preventDefault();
+      currentTarget = this;
+    } else {
+      touchTimeout = setTimeout(function() {
+        currentTarget = null;
+        touchTimeout = null;
+       isDoubleClick=false;
+        
+      }, 150);
+    }
+    startX = touch.clientX - this.offsetLeft;
+    startY = touch.clientY - this.offsetTop;
+  });
+
+
+  document.addEventListener('touchstart', function(e) {
+     if(isDoubleClick == true){
+      const touch = e.changedTouches[0];
+      const x = touch.clientX - startX;
+      const y = touch.clientY - startY;
+      currentTarget.style.left = x + 'px';
+      currentTarget.style.top = y + 'px';
+  }
+});
+  // Обработчик события touchmove
+  target.addEventListener('touchmove', function(e) {
+     if (isDoubleClick == false){
+    e.preventDefault();
+
+    
+
+    const touch = e.changedTouches[0];
+    const x = touch.clientX - startX;
+    const y = touch.clientY - startY;
+
+    // Перемещаем элемент на новую позицию
+    this.style.left = x + 'px';
+    this.style.top = y + 'px';
+    }
+   }
+  );
+
+  
+  target.addEventListener('touchend', function(e) {
+   console.log(isDoubleClick);
+  });
+
+  target.addEventListener('touchcancel', function(e) {
+    if (currentTarget !== null) {
+      currentTarget.style.left = startX + 'px';
+      currentTarget.style.top = startY + 'px';
+    }
+  });
+});
